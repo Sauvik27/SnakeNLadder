88603cc1aecc3c6e7eac830caec9a96a9c283f6a
Sauvik Kumar <57688017+Sauvik27@users.noreply.github.com>
2021-09-23 19:59:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,38 +3,51 @@ let temp = 1;
 let i = 1;
 function start(){
     let gridSize = parseInt(document.getElementById("grid").value);
-    let nPlayers = parseInt(document.getElementById("nPlayers").value);
+    let nPlayers = parseInt(document.getElementById("nPlayers").value)+1;
     let droll = [nPlayers];
-    let drhis = new Array(nPlayers);
-    let phis = new Array(nPlayers);
-
-    for(let j=0;j<phis.length;j++){
-        phis[i] = new Array();
-    }
-    for(let j=0;j<drhis.length;j++){
-        drhis[i] = new Array();
-    }
+    let drhis = [nPlayers];
+    let phis =  [nPlayers];
+    let prev = [nPlayers];
     while(temp === 1){
-        for(var i=0;i<nPlayers;i++){
-            droll.push(Math.floor(Math.random()*6)+1);
-            if(phis[i][phis[i].length-1]+droll < gridSize*gridSize || flag === 0){
-                drhis[i][drhis[i].length-1] = droll;
-                document.querySelector(`.r${i+1}`).innerHTML = drhis;
-            }
-            if(flag === 0){
-                phis[i][phis[i].length-1] = droll;
+        for(let i=0;i<nPlayers;i++){
+            droll[i] = Math.floor(Math.random()*6)+1;
+            if(flag === 0 || prev[i] === undefined){
+                drhis[i] =  droll[i];
             }
             else{
-                if(phis[i][phis[i].length-1] + droll <= gridSize*gridSize){
-                    phis[i][phis[i].length-1] = phis[i][phis[i].length-2] + droll;
-                    if(phis[i][phis[i].length-1] === gridSize*gridSize){
-                        document.querySelector('.winner').innerHTML = `Winner is Player 1 ${i+1}`;
+                if(prev[i] < gridSize*gridSize){
+                    drhis[i] = drhis[i] + " , " + droll[i];
+                }
+            }
+            if(flag === 0){
+                phis[i] = droll[i];
+                prev[i] = parseInt(droll[i]);
+                
+            }
+            else{
+                if(prev[i] + parseInt(droll[i]) <= gridSize*gridSize){
+                    phis[i] = phis[i] + " , " + (prev[i] + parseInt(droll[i]));
+                    prev[i] = prev[i] + parseInt(droll[i]);
+                    if(prev[i] === gridSize*gridSize){
+                        document.querySelector('.winner').innerHTML = `Winner is Player ${i}`;
                         temp = 0;
                         break;
                     }
                 }
             }
         }
-        i++;
+        flag = 1;
+        console.log(prev);
+
     }
+    for(var i=1;i<=nPlayers;i++){
+        document.querySelector(`.p${i}`).innerHTML = phis[i];
+    }
+    for(var i=1;i<=nPlayers;i++){
+        document.querySelector(`.d${i}`).innerHTML = droll[i];
+    }
+    for(var i=1;i<=nPlayers;i++){
+        document.querySelector(`.r${i}`).innerHTML = drhis[i];
+    }
+    
 }
